76f9cccf6e59c526d5d7db4d3f0e37473050f517
hitimr <hitimr@gmail.com>
2022-02-11 17:13:11 +0100

    renaming

---
+++ raytracing.cpp
@@ -0,0 +1,34 @@
+#include <openvdb/Types.h>
+#include <openvdb/math/Ray.h>
+#include <openvdb/openvdb.h>
+#include <openvdb/tools/LevelSetSphere.h>
+#include <openvdb/tools/RayIntersector.h>
+#include <openvdb/tools/RayTracer.h>
+#include <openvdb/tools/VolumeToMesh.h>
+#include <vector>
+
+using namespace openvdb;
+
+int main()
+{
+  openvdb::initialize();
+  // Create a FloatGrid and populate it with a narrow-band
+  // signed distance field of a sphere.
+  const float r =3;
+  const Vec3f c(0, 0, 0);
+  const float s = 0.5f, w = 2;
+
+  openvdb::FloatGrid::Ptr ls = tools::createLevelSetSphere<FloatGrid>(r, c, s, w);
+
+  openvdb::tools::LevelSetRayIntersector<FloatGrid> lsri(*ls);
+
+  const Vec3f dir(1.0, 0.0, 0.0);
+  const Vec3f eye(2.0, 0.0, 0.0);
+  const math::Ray<float> ray(eye, dir);
+
+  Real time = 0;
+  // lsri.intersectsWS(ray)
+
+  // Create a VDB file object and write out the grid.
+  openvdb::io::File("mygrids.vdb").write({ls});
+}
