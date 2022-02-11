fc914278dc11b2b8f93270fc28a63cfbb358bd9a
hitimr <hitimr@gmail.com>
2022-02-11 14:26:47 +0100

    better build process

--- install.sh
+++ install.sh
@@ -1,15 +1,43 @@
+# TODO: install OVDB in dir that does not require root
+
+# Environment variables
+export DIR_PROJECT_ROOT=$(pwd)
+export DIR_BUILD=$DIR_PROJECT_ROOT/build
+
+export DIR_OPENVDB=$DIR_PROJECT_ROOT/openvdb
+export DIR_OPENVDB_BUILD=$DIR_OPENVDB/build
+export DIR_BOOST=$DIR_PROJECT_ROOT/boost
+
+
+
 # install dependencies
-sudo apt-get install -y libboost-iostreams-dev
-sudo apt-get install -y libtbb-dev
-sudo apt-get install -y libblosc-dev
-sudo apt-get install -y libpthread-stubs0-dev
-sudo apt-get install -y libgtest-dev
+sudo apt-get install -y \
+    libboost-iostreams-dev \
+    libtbb-dev \
+    libblosc-dev \
+    libpthread-stubs0-dev \
+    libgtest-dev \
+    doxygen \
+    libgl1-mesa-dev \
+    libglfw3-dev \
+    libopengl-dev
+    
+# Tools
+sudo apt install paraview
 
 # build openvdb
 git clone https://github.com/AcademySoftwareFoundation/openvdb
-cd openvdb
-mkdir build
-cd build
-cmake ..
-make -j4
-sudo make install
+git pull
+mkdir -p $DIR_OPENVDB_BUILD
+( \
+    cd $DIR_OPENVDB_BUILD \
+    && cmake \
+    -D OPENVDB_BUILD_UNITTESTS=ON \
+    -D OPENVDB_BUILD_PYTHON_MODULE=ON \
+    -D OPENVDB_BUILD_VDB_RENDER=ON \
+    .. \
+    && make -j6 \
+    && sudo make install \
+)
+#make -j8 openvdb
+#sudo make install
