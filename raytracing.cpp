231ac5d3f80a3bd12632aa5aaaf26fdd0c893539
hitimr <hitimr@gmail.com>
2022-02-13 21:59:52 +0100

    compiles

--- raytracing.cpp
+++ raytracing.cpp
@@ -1,33 +1,59 @@
+#include <openvdb/Exceptions.h>
 #include <openvdb/Types.h>
 #include <openvdb/math/Ray.h>
+#include <openvdb/math/Transform.h>
 #include <openvdb/openvdb.h>
 #include <openvdb/tools/LevelSetSphere.h>
 #include <openvdb/tools/RayIntersector.h>
-#include <openvdb/tools/RayTracer.h>
-#include <openvdb/tools/VolumeToMesh.h>
+#include <openvdb/tools/RayTracer.h> // for Film
+
+
+#include <iostream>
 #include <vector>
 
+#define FP_TYPE float;
+
 using namespace openvdb;
+typedef math::Ray<double> RayT;
+typedef RayT::Vec3Type Vec3T;
+//typedef float Real;
+
+
+//std::vector<FP_TYPE> linspace(FP_TYPE )
 
 int main()
 {
   openvdb::initialize();
   // Create a FloatGrid and populate it with a narrow-band
   // signed distance field of a sphere.
-  const float r =3;
+  const float r = 5;
   const Vec3f c(0, 0, 0);
-  const float s = 0.5f, w = 2;
+  const float s = 0.1f;
+  const float w = 2;
 
+  // outer sphere
   openvdb::FloatGrid::Ptr ls = tools::createLevelSetSphere<FloatGrid>(r, c, s, w);
 
+  // intersector
   openvdb::tools::LevelSetRayIntersector<FloatGrid> lsri(*ls);
 
-  const Vec3f dir(1.0, 0.0, 0.0);
-  const Vec3f eye(2.0, 0.0, 0.0);
-  const math::Ray<float> ray(eye, dir);
+/*
+  int n_rays = 12;
+  for(int i = 0; i < n_rays; i++)
+  {
+    FP_TYPE x 
+  }
+*/
+  Vec3f dir(1.0, 0.0, 0.0);
+  dir = dir / dir.length();
+  Vec3f eye(0.5, 0.5, 0.0);
+  math::Ray<Real> ray(eye, dir);
 
+  Vec3T xyz(0);
   Real time = 0;
-  // lsri.intersectsWS(ray)
+  lsri.intersectsWS(ray, xyz);
+  std::cout << xyz << std::endl;
+
 
   // Create a VDB file object and write out the grid.
   openvdb::io::File("mygrids.vdb").write({ls});
