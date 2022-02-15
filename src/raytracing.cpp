2661c139b8b761a04f9b2ab715726275cc0d95ef
hitimr <hitimr@gmail.com>
2022-02-15 11:24:53 +0100

    added logging

--- src/raytracing.cpp
+++ src/raytracing.cpp
@@ -11,10 +11,11 @@
 // Boost
 #include <boost/program_options.hpp>
 
+
 // Custom
 #include "common.hpp"
-#include "util/timer.hpp"
 #include "util/misc.hpp"
+#include "util/timer.hpp"
 
 // Standard Library
 #include <cassert>
@@ -25,17 +26,15 @@
 using namespace openvdb;
 namespace po = boost::program_options;
 
-
-void benchmark(RayIntersectorT &lsri, int n_rays, RealT voxel_size, RealT radius)
-{
+void benchmark(RayIntersectorT &lsri, int n_rays, RealT voxel_size,
+               RealT radius) {
   // generate a circular range of rays with origin at 0,0,0
   // all rays point along the x-y-Plane. z is kept at 0 for now
   Vec3T eye({0, 0, 0}); // all rays have a common origin
   std::vector<RayT> rays(n_rays);
   std::vector<Vec3T> reference_solutions(n_rays);
-  std::vector<RealT> alpha_vals = linspace(0.0,  2.0 * M_PI, n_rays);
-  for (size_t i = 0; i < n_rays; i++)
-  {
+  std::vector<RealT> alpha_vals = linspace(0.0, 2.0 * M_PI, n_rays);
+  for (size_t i = 0; i < n_rays; i++) {
     Vec3T direction({
         math::Cos(alpha_vals[i]), // x = Cos(α)
         math::Sin(alpha_vals[i]), // y = Sin(α)
@@ -45,7 +44,8 @@ void benchmark(RayIntersectorT &lsri, int n_rays, RealT voxel_size, RealT radius
     direction.normalize();
     rays[i] = RayT(eye, direction);
 
-    Vec3T solution({radius * math::Cos(alpha_vals[i]), radius * math::Sin(alpha_vals[i]), 0});
+    Vec3T solution({radius * math::Cos(alpha_vals[i]),
+                    radius * math::Sin(alpha_vals[i]), 0});
     reference_solutions[i] = solution;
   }
 
@@ -54,8 +54,7 @@ void benchmark(RayIntersectorT &lsri, int n_rays, RealT voxel_size, RealT radius
   // Run Benchmark
   Timer timer;
   timer.reset();
-  for (size_t i = 0; i < n_rays; i++)
-  {
+  for (size_t i = 0; i < n_rays; i++) {
     lsri.intersectsWS(rays[i], calculated[i]);
   }
   double time = timer.get();
@@ -63,8 +62,7 @@ void benchmark(RayIntersectorT &lsri, int n_rays, RealT voxel_size, RealT radius
   // Verify Solution
   RealT eps = voxel_size / 2;
   Vec3T vec_eps(eps, eps, eps);
-  for (size_t i = 0; i < n_rays; i++)
-  {
+  for (size_t i = 0; i < n_rays; i++) {
     assert(math::isApproxEqual(calculated[i], reference_solutions[i], vec_eps));
   }
 
@@ -74,46 +72,49 @@ void benchmark(RayIntersectorT &lsri, int n_rays, RealT voxel_size, RealT radius
    */
 
   // results for each ray
-  if (true) // TODO: replace with boost logger
-  {
-    std::cout << "Benchmark finished" << std::endl;
-    std::cout << "Voxel size: " << voxel_size << std::endl;
-    std::cout << "Calculated | reference" << std::endl;
-    for (size_t i = 0; i < n_rays; i++)
-    {
-      std::cout << "Ray " << i << std::endl;
-      std::cout << "x: " << calculated[i].x() << "|" << reference_solutions[i].x() << std::endl;
-      std::cout << "y: " << calculated[i].y() << "|" << reference_solutions[i].y() << std::endl;
-      std::cout << "z: " << calculated[i].z() << "|" << reference_solutions[i].z() << std::endl;
-      std::cout << std::endl;
-    }
+  
+  PLOG_INFO<< "Benchmark finished" << std::endl;
+  /*
+  LOG_DEBUG << "Voxel size: " << voxel_size << std::endl;
+  LOG_DEBUG << "Calculated | reference" << std::endl;
+  for (size_t i = 0; i < n_rays; i++) {
+    LOG_DEBUG << "Ray " << i << std::endl;
+    LOG_DEBUG << "x: " << calculated[i].x() << "|" << reference_solutions[i].x()
+              << std::endl;
+    LOG_DEBUG << "y: " << calculated[i].y() << "|" << reference_solutions[i].y()
+              << std::endl;
+    LOG_DEBUG << "z: " << calculated[i].z() << "|" << reference_solutions[i].z()
+              << std::endl;
+    LOG_DEBUG << std::endl;
   }
+  */
 }
 
-po::variables_map parse_options(int ac, char **av)
-{
+po::variables_map parse_options(int ac, char **av) {
   po::options_description desc("Allowed options");
 
-    desc.add_options()
-    ("help,h", "produce help message")
-    ("nbench,nb", po::value<int>()->default_value(DEFAULT_NBENCH), "number of points for the benchmark")
-    ;
+  desc.add_options()("help,h", "produce help message")(
+      "nbench,nb", po::value<int>()->default_value(DEFAULT_NBENCH),
+      "number of points for the benchmark");
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(ac, av, desc), vm);
-    po::notify(vm);
+  po::variables_map vm;
+  po::store(po::parse_command_line(ac, av, desc), vm);
+  po::notify(vm);
 
-    if (vm.count("help"))
-    {
-      std::cout << desc << "\n";
-      exit(0);
-    }
-    return vm;
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    exit(0);
+  }
+  return vm;
 }
 
-int main(int ac, char **av)
-{
+int main(int ac, char **av) {
+
+    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
+    plog::init(plog::verbose, &consoleAppender);
+  
   po::variables_map options = parse_options(ac, av);
+
   openvdb::initialize();
 
   // Create Level Set sphere
@@ -123,12 +124,12 @@ int main(int ac, char **av)
   const Vec3f center(0, 0, 0);
   const float voxel_size = 0.01f;
   const float half_width = 2;
-  openvdb::FloatGrid::Ptr ls =
-      tools::createLevelSetSphere<FloatGrid>(radius,     // radius of the sphere in world units
-                                             center,     // center of the sphere in world units
-                                             voxel_size, // voxel size in world units
-                                             half_width  // half the width of the narrow band, in voxel units
-      );
+  openvdb::FloatGrid::Ptr ls = tools::createLevelSetSphere<FloatGrid>(
+      radius,     // radius of the sphere in world units
+      center,     // center of the sphere in world units
+      voxel_size, // voxel size in world units
+      half_width  // half the width of the narrow band, in voxel units
+  );
 
   // intersector
   RayIntersectorT lsri(*ls);
