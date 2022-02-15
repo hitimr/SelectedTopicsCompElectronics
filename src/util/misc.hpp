37b7a7a33ee5598220ebd756eef68424d3e818f5
hitimr <hitimr@gmail.com>
2022-02-15 10:08:25 +0100

    added program options

--- src/util/misc.hpp
+++ src/util/misc.hpp
@@ -1,16 +1,2 @@
 #include <vector>
 
-
-template<typename T>
-std::vector<T> linspace(T start, T end, size_t count)
-{
-  assert(end - start != 0);
-  T step = (end - start) / count;
-  std::vector<T> ret_vals(count);
-
-  for (int i = 0; i < count; i++)
-  {
-    ret_vals[i] = start + i * step;
-  }
-  return ret_vals;
-}
