522a4c5b31439cc8c082fc4446819e8d71c96841
hitimr <hitimr@gmail.com>
2022-02-15 11:24:53 +0100

    added logging

--- src/common.hpp
+++ src/common.hpp
@@ -2,12 +2,15 @@
 #define DEFAULT_NRAYS_START 10
 #define DEFAULT_NRAYS_END 100
 
-
-
 // Used Types
 typedef double RealT;
 typedef openvdb::math::Ray<double> RayT;
 typedef RayT::Vec3Type Vec3T;
 typedef openvdb::tools::LevelSetRayIntersector<openvdb::FloatGrid> RayIntersectorT;
- 
 
+
+// Logging
+#include <plog/Log.h>
+#include <plog/Init.h>
+#include <plog/Formatters/TxtFormatter.h>
+#include <plog/Appenders/ColorConsoleAppender.h>
