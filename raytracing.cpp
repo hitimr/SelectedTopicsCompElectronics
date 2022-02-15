eeb4764217d32aaeb691f4a73761e75c0d461c50
hitimr <hitimr@gmail.com>
2022-02-15 06:57:42 +0100

    values seem ok

--- raytracing.cpp
+++ raytracing.cpp
@@ -13,7 +13,7 @@
 
 typedef float RealT;
 typedef openvdb::math::Ray<double> RayT;
-typedef RayT::Vec3Type     Vec3T;
+typedef RayT::Vec3Type Vec3T;
 
 using namespace openvdb;
 
@@ -32,15 +32,17 @@ std::vector<RealT> linspace(RealT start, RealT end, size_t count)
 
 int main()
 {
+  /**
+   * Init
+   * 
+   */
   openvdb::initialize();
-  // Create a FloatGrid and populate it with a narrow-band
-  // signed distance field of a sphere.
+
+  // Create Level Set sphere
   const RealT r = 5;
   const Vec3f c(0, 0, 0);
   const float s = 0.1f;
   const float w = 2;
-
-  // outer sphere
   openvdb::FloatGrid::Ptr ls = tools::createLevelSetSphere<FloatGrid>(r, c, s, w);
 
   // intersector
@@ -48,8 +50,9 @@ int main()
 
   // generate a circular range of rays with origin at 0,0,0
   size_t n_rays = 12;
-  Vec3T origin({0, 0, 0}); // all rays have a common origin
+  Vec3T eye({0, 0, 0}); // all rays have a common origin
   std::vector<RayT> rays(n_rays);
+  std::vector<Vec3T> reference_solutions(n_rays);
   std::vector<RealT> alpha_vals = linspace(0, 2 * M_PI, n_rays);
   for (size_t i = 0; i < n_rays; i++)
   {
@@ -58,25 +61,40 @@ int main()
         math::Sin(alpha_vals[i]), // y = Sin(α)
         0                         // z = 0
     });
-    rays[i] = RayT(direction, direction);
+
+    direction.normalize();
+    rays[i] = RayT(eye, direction);
+
+    Vec3T solution({r * math::Cos(alpha_vals[i]), r * math::Sin(alpha_vals[i]), 0});
+    reference_solutions[i] = solution;
   }
-  /*
-    // calculate interse4ctions
-    std::vector<Vec3T> intersections(n_rays, Vec3T(0,0,0));
-    for (size_t i = 0; i < n_rays; i++)
-    {
-      Vec3T xyz(0);
-      lsri.intersectsWS(xyz);
-    }
-  */
 
-  const Vec3T dir(1.0, 0.0, 0.0);
-  const Vec3T eye(2.0, 0.0, 0.0);
-  RayT ray(eye, dir);
+  /**
+   * Benchmark
+   * 
+   */
+  std::vector<Vec3T> calculated(n_rays, Vec3T(0, 0, 0));
+  for (size_t i = 0; i < n_rays; i++)
+  {
+    lsri.intersectsWS(rays[i], calculated[i]);
+  }
 
-  Vec3T xyz(0);
-  lsri.intersectsWS(ray, xyz);
-  std::cout << xyz << std::endl;
+
+  /**
+   * Summary
+   * 
+   */
+  std::cout << "Benchmark finished" << std::endl;
+  for (size_t i = 0; i < n_rays; i++)
+  {
+    std::cout << "Ray " << i << std::endl;
+    std::cout << "x: " << calculated[i].x() << "|" << reference_solutions[i].x() << std::endl; 
+    std::cout << "y: " << calculated[i].y() << "|" << reference_solutions[i].y() << std::endl; 
+    std::cout << std::endl;
+  }
+  
+
+  //
 
   // Create a VDB file object and write out the grid.
   openvdb::io::File("mygrids.vdb").write({ls});
