bdcea5faa13ad22e8341d6349f62c3d9f4aa2b3d
hitimr <hitimr@gmail.com>
2022-02-15 12:07:42 +0100

    better logging

--- src/common.hpp
+++ src/common.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #define DEFAULT_NBENCH 10
 #define DEFAULT_NRAYS_START 10
 #define DEFAULT_NRAYS_END 100
@@ -8,9 +9,27 @@ typedef openvdb::math::Ray<double> RayT;
 typedef RayT::Vec3Type Vec3T;
 typedef openvdb::tools::LevelSetRayIntersector<openvdb::FloatGrid> RayIntersectorT;
 
-
 // Logging
 #include <plog/Log.h>
-#include <plog/Init.h>
-#include <plog/Formatters/TxtFormatter.h>
 #include <plog/Appenders/ColorConsoleAppender.h>
+#include <plog/Initializers/RollingFileInitializer.h>
+
+
+class CustomPlogFormatter
+{
+public:
+  static plog::util::nstring
+  header() // This method returns a header for a new file. In our case it is empty.
+  {
+    return plog::util::nstring();
+  }
+
+  static plog::util::nstring
+  format(const plog::Record &record) // This method returns a string from a record.
+  {
+    plog::util::nostringstream ss;
+    ss << record.getMessage(); // Produce a simple string with a log message.
+
+    return ss.str();
+  }
+};
