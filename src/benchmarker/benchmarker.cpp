b2e8ea782ad12b580d9ab9950596029c697b5f6c
hitimr <hitimr@gmail.com>
2022-02-17 14:12:30 +0100

    compiles but TreeT is hardcoded to double

--- src/benchmarker/benchmarker.cpp
+++ src/benchmarker/benchmarker.cpp
@@ -16,6 +16,17 @@ template <class FP_Type> Benchmarker<FP_Type>::Benchmarker(OptionsT options)
   // Create Level Set sphere
   // for details see:
   // https://www.openvdb.org/documentation/doxygen/namespaceopenvdb_1_1v8__0_1_1tools.html#a47e7b3c363d0d3a15b5859c4b06e9d8b
-  // const Vec3T center(0, 0, 0);
-  // const FP_Type half_width = 2;
+  const Vec3T center(0, 0, 0);
+  const FP_Type half_width = 2;
+
+
+
+  GridT::Ptr ls = openvdb::tools::createLevelSetSphere<GridT>(
+      options["radius"].as<FP_Type>(),     // radius of the sphere in world units
+      center,                              // center of the sphere in world units
+      options["voxel_size"].as<FP_Type>(), // voxel size in world units
+      half_width                           // half the width of the narrow band, in voxel units
+  );
+
+  RayIntersectorT lsri(*ls);
 }
