6a0bc37af92e2ecaf7716c33265e563815ea4ba3
hitimr <hitimr@gmail.com>
2022-02-17 15:21:13 +0100

    ported up to ray generation

--- src/benchmarker/benchmarker.hpp
+++ src/benchmarker/benchmarker.hpp
@@ -1,14 +1,7 @@
 #pragma once
-
-// OpenVDB
-#include <openvdb/Exceptions.h>
 #include <openvdb/Types.h>
 #include <openvdb/math/Ray.h>
-#include <openvdb/math/Transform.h>
 #include <openvdb/openvdb.h>
-#include <openvdb/tools/LevelSetSphere.h>
-#include <openvdb/tools/RayIntersector.h>
-#include <openvdb/tools/RayTracer.h> // for Film
 
 // Boost
 #include <boost/program_options.hpp>
@@ -16,14 +9,23 @@
 template <class FP_Type = double> class Benchmarker
 {
   using OptionsT = boost::program_options::variables_map;
+  using Vec3T = openvdb::math::Vec3<FP_Type>;
+  using RayT = openvdb::math::Ray<FP_Type>;
+  using TreeT = typename openvdb::tree::Tree4<FP_Type, 5, 4, 3>::Type;
+  using GridT = openvdb::Grid<TreeT>; // Taken from openvdb.h
 
 private:
   // openvdb::FloatGrid::Ptr level_set;
+  std::vector<int> ray_vals;
+  const OptionsT & options;
+
+  const FP_Type pi = std::acos(-1);
 
 public:
   ~Benchmarker(){};
-  Benchmarker(OptionsT options);
-  Benchmarker(){};
+  Benchmarker(const OptionsT & options);
+
+  void run(size_t nrays);
 };
 
 // Valid instantiations
