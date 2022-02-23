47599a8661d5dc4b852108f548f21f0de694bfbd
hitimr <hitimr@gmail.com>
2022-02-23 18:51:20 +0100

    switched to computation of time

--- src/benchmarker/benchmarker.cpp
+++ src/benchmarker/benchmarker.cpp
@@ -30,16 +30,15 @@ Benchmarker::Benchmarker(const OptionsT &options) : options(options)
 {
 
   // Sphere Parameters
-  sphere_radius_outer = options["radius"].as<FP_Type>();
-  voxel_size = options["voxel_size"].as<FP_Type>();
+  sphere_radius_outer = (FP_Type) options["radius"].as<double>();
+  voxel_size = (FP_Type) options["voxel_size"].as<double>();
   level_set_half_width = 2.0;
 }
 
 void Benchmarker::run_openVDB(size_t n_rays)
 {
-
   assert(n_rays > 0);
-  PLOG_INFO << "\nRunning benchmark for " << n_rays << " Rays" << std::endl;
+  PLOG_INFO << "\nRunning OpenVDB benchmark for " << n_rays << " Rays" << std::endl;
 
   // generate Sphere
   OVBD_Vec3T center(0, 0, 0);
@@ -50,29 +49,28 @@ void Benchmarker::run_openVDB(size_t n_rays)
       level_set_half_width // half the width of the narrow band, in voxel units
   );
 
-  // Ray Intersector. Triple nested types. nice...
+  // Ray Intersector: Triple nested types. nice...
   tools::LevelSetRayIntersector<OVBD_GridT, tools::LinearSearchImpl<OVBD_GridT, 0, FP_Type>,
                                 OVBD_GridT::TreeType::RootNodeType::ChildNodeType::LEVEL, OVBD_RayT>
       ray_intersector(*level_set);
 
-  // generate a circular range of rays with origin at 0,0,0
-  // all rays point along the x-y-Plane. z is kept at 0 for now
+
   std::vector<OVBD_RayT> rays = generate_rays<OVBD_RayT>(n_rays);
   std::vector<OVBD_Vec3T> reference_solutions = calculate_reference_solution<OVBD_Vec3T>(n_rays);
 
   // Run Benchmark
-  std::vector<OVBD_Vec3T> calculated(n_rays, OVBD_Vec3T(0, 0, 0)); // results
+  std::vector<FP_Type> calculated_time(n_rays);
   Timer timer;
   timer.reset();
   for (size_t i = 0; i < n_rays; i++)
   {
-    ray_intersector.intersectsWS(rays[i], calculated[i]);
+    ray_intersector.intersectsWS(rays[i], calculated_time[i]);
   }
 
   double time = timer.get();
   PLOG_INFO << "OpenVDB Finished in " << time << "s (" << (double)n_rays / (1000 * time)
             << " kRays/s)" << std::endl;
-
+  /*
   // results for each ray
   PLOG_DEBUG << "Voxel size: " << voxel_size << std::endl;
   PLOG_DEBUG << "Calculated | reference" << std::endl;
@@ -84,9 +82,9 @@ void Benchmarker::run_openVDB(size_t n_rays)
     PLOG_DEBUG << "z: " << calculated[i].z() << "|" << reference_solutions[i].z() << std::endl;
     PLOG_DEBUG << std::endl;
   }
+  */
 
   // Verify Solution
-  FP_Type voxel_size = options["voxel_size"].as<FP_Type>();
   FP_Type eps = voxel_size / 2;
   for (size_t i = 0; i < n_rays; i++)
   {
