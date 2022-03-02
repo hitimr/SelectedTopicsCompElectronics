53210eae3bf3b7852733221fb1b67657a7d2c4c1
hitimr <hitimr@gmail.com>
2022-03-03 00:02:18 +0100

    nanovdb datastructs are compiling in CUDA

--- src/benchmarker/nanoVDB_GPU.cu
+++ src/benchmarker/nanoVDB_GPU.cu
@@ -5,7 +5,16 @@
 
 #include <vector>
 
-void run_nanoVDB_GPU(nanovdb::GridHandle<nanovdb::CudaDeviceBuffer> &level_set, size_t n_rays)
+__global__ void run(nanovdb::Grid<nanovdb::NanoTree<FP_Type>> *d_level_set, size_t n_rays)
+{
+
+  auto acc = d_level_set->tree().getAccessor();
+  nanovdb::Coord ijk;
+  FP_Type t0;
+  FP_Type v;
+}
+
+void run_nanoVDB_GPU(nanovdb::GridHandle<nanovdb::CudaDeviceBuffer> &grid_handle, size_t n_rays)
 {
   using FP_Type = float;
   using RayT = nanovdb::Ray<FP_Type>;
@@ -21,8 +30,12 @@ void run_nanoVDB_GPU(nanovdb::GridHandle<nanovdb::CudaDeviceBuffer> &level_set, 
       calculate_reference_solution<Vec3T>(n_rays, sphere_radius_outer);
 
   // Send data to GPU
-  level_set.deviceUpload(); // TODO: move outside so it wont be called every time
+  grid_handle.deviceUpload(); // TODO: move outside so it wont be called every time
+  nanovdb::Grid<nanovdb::NanoTree<FP_Type>> * d_grid_handle = grid_handle.deviceGrid<FP_Type>();
+  if (!d_grid_handle)
+    throw std::runtime_error("GridHandle does not contain a valid device grid");
 
   // Run Benchmark
   std::vector<Vec3T> calculated(n_rays, Vec3T(0, 0, 0)); // results
+  run<<<256, 256>>>(d_grid_handle, n_rays);
 }
