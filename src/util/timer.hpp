6a38c5a9f8761c406f93a53197c4187d5c1c3c7f
hitimr <hitimr@gmail.com>
2022-03-04 13:30:04 +0100

    expanded timer class

--- src/util/timer.hpp
+++ src/util/timer.hpp
@@ -1,25 +1,67 @@
 #pragma once
+
+#include <cassert>
 #include <chrono>
+#include <cmath>
 #include <iostream>
- 
+#include <vector>
 
 class Timer
 {
 public:
-  Timer() {
-    reset();
-  }
+  Timer(bool store_timings = false) : m_store_timings(store_timings) { reset(); }
 
   void reset() { chrono_ts = std::chrono::high_resolution_clock::now(); }
 
-  double get() const
+  double get()
   {
     auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - chrono_ts);
-    return duration.count() * 1e-9;
+    double duration =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - chrono_ts).count() * 1e-9;
+
+    if (m_store_timings)
+    {
+      m_timings.push_back(duration);
+    }
+
+    return duration;
   }
 
-private:
-  double ts;
+  double median()
+  {
+    assert(m_timings.size() > 0);
+
+    return m_timings[m_timings.size() / 2];
+  }
+
+  double average()
+  {
+    assert(m_timings.size() > 0);
+
+    double sum = 0;
+    for (auto timing : m_timings)
+    {
+      sum += timing;
+    }
+    return sum / (double)m_timings.size();
+  }
+
+  double std_dev()
+  {
+    double avg = average();
+    double sigma2 = 0;
+    for (size_t i = 0; i < m_timings.size(); i++)
+    {
+      sigma2 += (m_timings[i] - avg) * (m_timings[i] - avg);
+    }
+
+    return sqrt(sigma2) / (double)m_timings.size();
+  }
+
+  double m_ts;
+  bool m_store_timings;
+  std::vector<double> m_timings;
   std::chrono::time_point<std::chrono::high_resolution_clock> chrono_ts;
 };
+
+
